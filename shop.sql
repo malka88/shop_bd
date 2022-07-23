5a14e12fe93db33704a1da0cde550b98b70a89ea
malka88 <malka_88@outlook.com>
2022-07-23 12:48:10 +0700

    Add files via upload

--- shop.sql
+++ shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Июл 22 2022 г., 18:56
+-- Время создания: Июл 23 2022 г., 07:27
 -- Версия сервера: 10.4.24-MariaDB
 -- Версия PHP: 8.1.6
 
@@ -30,19 +30,18 @@ SET time_zone = "+00:00";
 CREATE TABLE `category` (
   `category_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `count` int(11) DEFAULT NULL
+  `description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Дамп данных таблицы `category`
 --
 
-INSERT INTO `category` (`category_id`, `name`, `description`, `count`) VALUES
-(1, 'Блузы и рубашки', 'Блузы и рубашки...', 5),
-(2, 'Брюки', 'Брюки...', 10),
-(3, 'Платья и сарафаны', 'Платья и сарафаны...', 2),
-(4, 'Юбки', 'Юбки...', NULL);
+INSERT INTO `category` (`category_id`, `name`, `description`) VALUES
+(1, 'Блузы и рубашки', 'Блузы и рубашки...'),
+(2, 'Брюки', 'Брюки...'),
+(3, 'Платья и сарафаны', 'Платья и сарафаны...'),
+(4, 'Юбки', 'Юбки...');
 
 -- --------------------------------------------------------
 
@@ -53,17 +52,16 @@ INSERT INTO `category` (`category_id`, `name`, `description`, `count`) VALUES
 CREATE TABLE `picture` (
   `picture_id` int(11) NOT NULL,
   `path` text NOT NULL,
-  `alt` varchar(255) NOT NULL,
-  `is_main` tinyint(1) NOT NULL
+  `alt` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Дамп данных таблицы `picture`
 --
 
-INSERT INTO `picture` (`picture_id`, `path`, `alt`, `is_main`) VALUES
-(1, 'assets/1/1.png', 'Блуза', 1),
-(2, 'assets/1/2.png', 'Блуза', 0);
+INSERT INTO `picture` (`picture_id`, `path`, `alt`) VALUES
+(1, 'assets/1/1.png', 'Блуза'),
+(2, 'assets/1/2.png', 'Блуза');
 
 -- --------------------------------------------------------
 
@@ -107,30 +105,31 @@ INSERT INTO `product` (`product_id`, `name`, `description`, `is_active`) VALUES
 
 CREATE TABLE `product_and_category` (
   `category_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL
+  `product_id` int(11) NOT NULL,
+  `is_main` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Дамп данных таблицы `product_and_category`
 --
 
-INSERT INTO `product_and_category` (`category_id`, `product_id`) VALUES
-(1, 1),
-(1, 2),
-(1, 3),
-(2, 4),
-(2, 5),
-(2, 6),
-(2, 7),
-(2, 8),
-(2, 9),
-(2, 10),
-(2, 11),
-(2, 12),
-(2, 13),
-(3, 3),
-(3, 14),
-(3, 15);
+INSERT INTO `product_and_category` (`category_id`, `product_id`, `is_main`) VALUES
+(1, 1, 1),
+(1, 2, 1),
+(1, 3, 1),
+(2, 4, 1),
+(2, 5, 1),
+(2, 6, 1),
+(2, 7, 1),
+(2, 8, 1),
+(2, 9, 1),
+(2, 10, 1),
+(2, 11, 1),
+(2, 12, 1),
+(2, 13, 1),
+(3, 3, 0),
+(3, 14, 1),
+(3, 15, 1);
 
 -- --------------------------------------------------------
 
@@ -140,17 +139,18 @@ INSERT INTO `product_and_category` (`category_id`, `product_id`) VALUES
 
 CREATE TABLE `product_and_picture` (
   `product_id` int(11) NOT NULL,
-  `picture_id` int(11) NOT NULL
+  `picture_id` int(11) NOT NULL,
+  `is_main` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Дамп данных таблицы `product_and_picture`
 --
 
-INSERT INTO `product_and_picture` (`product_id`, `picture_id`) VALUES
-(1, 1),
-(1, 2),
-(2, 1);
+INSERT INTO `product_and_picture` (`product_id`, `picture_id`, `is_main`) VALUES
+(1, 1, 1),
+(1, 2, 0),
+(2, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -160,9 +160,9 @@ INSERT INTO `product_and_picture` (`product_id`, `picture_id`) VALUES
 
 CREATE TABLE `product_price` (
   `product_id` int(11) NOT NULL,
-  `price` int(11) NOT NULL,
-  `price_discount` int(11) DEFAULT NULL,
-  `price_promo` int(11) DEFAULT NULL
+  `price` decimal(10,2) NOT NULL,
+  `price_discount` decimal(10,2) DEFAULT NULL,
+  `price_promo` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -170,8 +170,8 @@ CREATE TABLE `product_price` (
 --
 
 INSERT INTO `product_price` (`product_id`, `price`, `price_discount`, `price_promo`) VALUES
-(1, 3000, 1500, 1000),
-(2, 6000, NULL, NULL);
+(1, '3000.00', '1500.00', '1000.00'),
+(2, '6000.00', NULL, NULL);
 
 --
 -- Индексы сохранённых таблиц
